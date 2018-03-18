e9144eb81c7dd92752679d5395e809541f11a0f8
lxdnz254 <lxdnz34@gmail.com>
2018-03-18 15:41:50 +1300

    Updated Map options and style, refactored css files

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
+import './css/MapContainer.css'
 
 const AnyReactComponent = ({ text }) => <div>{ text }</div>;
 
+
 export default class MapContainer extends Component {
     static defaultProps = {
-        style: {width: '100%', height: '500px'},
         center: { lat: -38.560926, lng: 174.983468 },
-        zoom: 15
+        zoom: 14,
+        options: {mapTypeControl: true,
+            styles: [{ stylers: [{ 'saturation': -100 }, { 'gamma': 0.8 }, { 'lightness': 4 }, { 'visibility': 'on' }] }]
+        }
     };
 
     render() {
         return (
-            <div className='google-map' style={this.props.style}>
+            <div className='google-map'>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: [process.env.REACT_APP_GOOGLE_MAP_API_KEY] }}
                     defaultCenter={ this.props.center }
-                    defaultZoom={ this.props.zoom }>
+                    defaultZoom={ this.props.zoom }
+                    options = {this.props.options}
+                    >
                     <AnyReactComponent
                         lat={ -38.560926 }
                         lng={ 174.983468 }
